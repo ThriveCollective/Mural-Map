2b92d8ab589fba27ebd1580037518e6565bbd826
immortalstudio3103-design <immortalstudio3103@gmail.com>
2026-03-24 14:17:05 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,7 @@
 window.MURAL_MAP_CONFIG = {
   // Replace [YOUR_SHEET_ID] with the long string of letters/numbers in your Sheet URL
   CSV_URL: "https://docs.google.com/spreadsheets/d/[1534722208]/export?format=csv",
+}
   
   DEFAULT_CENTER: { lat: 40.7128, lng: -74.006 },
   DEFAULT_ZOOM: 11
